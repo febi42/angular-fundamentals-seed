8bf4b87aa92ba79ce904e2fb923fb7a6fcdca37a
Fabian Maeser <fabian.maeser@ergon.ch>
2020-03-20 13:41:52 +0100

    interpolation and expressions
    Signed-off-by: Fabian Maeser <fabian.maeser@ergon.ch>

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,12 +5,17 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      {{title}}
+      {{title + '!'}}
+      <div>{{numberOne + numberTwo}}</div>
+      <div>{{isHappy ? ':)' : ':('}}</div>
     </div>
   `
 })
 export class AppComponent {
   title: String;
+  numberOne: number = 1;
+  numberTwo: number = 2;
+  isHappy: boolean = true;
 
   constructor() {
     this.title = 'Ultimate Angular';
