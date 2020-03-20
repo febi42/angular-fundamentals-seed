df27948497432db6ff10e349581a823831b22248
Fabian Maeser <fabian.maeser@ergon.ch>
2020-03-20 14:03:36 +0100

    property binding
    Signed-off-by: Fabian Maeser <fabian.maeser@ergon.ch>

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      {{title + '!'}}
-      <div>{{numberOne + numberTwo}}</div>
-      <div>{{isHappy ? ':)' : ':('}}</div>
+      <h1 [innerHTML]="title"></h1>
+      <h1>{{ title }}</h1>
+      <img [src]="logo">
+      <input type="text" [value]="name">
+      <div>{{ name }}</div>
     </div>
   `
 })
 export class AppComponent {
-  title: String;
-  numberOne: number = 1;
-  numberTwo: number = 2;
-  isHappy: boolean = true;
+  title: string;
+  name: string = 'Todd';
+  logo: string = 'img/logo.svg';
 
   constructor() {
     this.title = 'Ultimate Angular';
