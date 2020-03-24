2e586844d523f9ce0526cee9f1eea606c4bbebab
Fabian Maeser <fabian.maeser@ergon.ch>
2020-03-24 12:00:32 +0100

    ngSubmit and stateless Output

--- app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
+++ app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { PassengerDashboardService } from '../../passenger-dashboard.service';
+import {PassengerDashboardService} from '../../passenger-dashboard.service';
 
-import { Passenger } from '../../models/passenger.interface';
+import {Passenger} from '../../models/passenger.interface';
 
 @Component({
   selector: 'passenger-viewer',
@@ -10,17 +10,29 @@ import { Passenger } from '../../models/passenger.interface';
   template: `
     <div>
       <passenger-form
-        [detail]="passenger">
+        [detail]="passenger"
+        (update)="onUpdatePassenger($event)">
       </passenger-form>
     </div>
   `
 })
 export class PassengerViewerComponent implements OnInit {
   passenger: Passenger;
-  constructor(private passengerService: PassengerDashboardService) {}
+
+  constructor(private passengerService: PassengerDashboardService) {
+  }
+
   ngOnInit() {
     this.passengerService
       .getPassenger(1)
       .subscribe((data: Passenger) => this.passenger = data);
+  }
+
+  onUpdatePassenger(event: Passenger) {
+    this.passengerService
+      .updatePassenger(event)
+      .subscribe((data: Passenger) => {
+        this.passenger = Object.assign({}, this.passenger, data);
+      });
   }
 }
