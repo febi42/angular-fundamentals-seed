ce23218b02ac804efc42cc61956c2b2353e32282
Fabian Maeser <fabian.maeser@ergon.ch>
2020-03-24 09:23:40 +0100

    Binding to radio buttons
    Signed-off-by: Fabian Maeser <fabian.maeser@ergon.ch>

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
-import { Passenger } from '../../models/passenger.interface';
+import {Passenger} from '../../models/passenger.interface';
 
 @Component({
   selector: 'passenger-form',
@@ -25,6 +25,35 @@ import { Passenger } from '../../models/passenger.interface';
           [ngModel]="detail?.id">
       </div>
 
+      <div>
+        <label>
+          <input
+            type="radio"
+            [value]="true"
+            name="checkedIn"
+            [ngModel]="detail?.checkedIn"
+            (ngModelChange)="toggleCheckIn($event)">
+          Yes
+        </label>
+        <label>
+          <input
+            type="radio"
+            [value]="false"
+            name="checkedIn"
+            [ngModel]="detail?.checkedIn"
+            (ngModelChange)="toggleCheckIn($event)">
+          No
+        </label>
+      </div>
+
+      <div *ngIf="form.value.checkedIn">
+        Check in date:
+        <input
+          type="number"
+          name="checkInDate"
+          [ngModel]="detail?.checkInDate">
+      </div>
+
       {{ form.value | json }}
     </form>
   `
@@ -32,4 +61,10 @@ import { Passenger } from '../../models/passenger.interface';
 export class PassengerFormComponent {
   @Input()
   detail: Passenger;
+
+  toggleCheckIn(checkedIn: boolean) {
+    if (checkedIn) {
+      this.detail.checkInDate = Date.now();
+    }
+  }
 }
